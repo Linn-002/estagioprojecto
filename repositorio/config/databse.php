fcea4679cef1b3ffd1354126bddc12c0ddd4ee89
Felicis-Dev011 <155689793+Felicis-Dev011@users.noreply.github.com>
2026-02-23 14:11:33 +0100

    novo2

---
+++ repositorio/config/databse.php
@@ -0,0 +1,15 @@
+<?
+
+$host = "localhost";
+$db   = "repositoriodb";
+$user = "root";
+$pass = "";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
+}
+
+session_start();
+?>
